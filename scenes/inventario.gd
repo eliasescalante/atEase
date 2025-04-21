75806aefd143600dc71c49b2e2269fe829ac9233
eliasescalante <eliasw.escalante@gmail.com>
2025-04-21 12:23:10 -0300

    agrego chech de inventario y cambio a escena final

--- scenes/inventario.gd
+++ scenes/inventario.gd
@@ -22,4 +22,15 @@ func addYerbaToInventory():
 	
 func addAzucareraToInventory():
 	azucarera.visible = true
-	
+
+func get_total_items() -> int:
+	var total = 0
+	if pava.visible:
+		total += 1
+	if mate.visible:
+		total += 1
+	if yerba.visible:
+		total += 1
+	if azucarera.visible:
+		total += 1
+	return total
