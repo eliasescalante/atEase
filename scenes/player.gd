3a2ce9fa872082f50cd1e1539bfc7adc1ce486ce
eliasescalante <eliasw.escalante@gmail.com>
2025-04-21 17:40:55 -0300

    arreglo a sprite nivel 1

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,10 +3,10 @@ extends Node2D
 @onready var sprite = $npc
 
 var sprite_textures = [
-	preload("res://assets/PJ 5.png"),      # Calmado
+	preload("res://assets/PJ 5.png"),      # Calmado (index 0)
 	preload("res://assets/PJ (3.png"),     # Menos calmado
-	preload("res://assets/PJ (4.png"),     # agitado
-	preload("res://assets/pj (2).png")     # mal agitadisimo
+	preload("res://assets/PJ (4.png"),     # Agitado
+	preload("res://assets/pj (2).png")     # Muy agitado
 ]
 
 var last_state := 0
@@ -15,16 +15,17 @@ func _ready():
 	update_sprite(0)
 
 func update_sprite(interpolation: float):
-	#inverti la logica asi me pega la respiracion
-	var npc_state = roundi((1.0 - interpolation) * (sprite_textures.size() - 1))
-	
-	if npc_state != last_state:
-		if npc_state > last_state:
-			AudioManager.play_respiracion_calmada()
-			AudioManager.stop_respiracion_rapida()
-		elif npc_state < last_state:
-			AudioManager.play_respiracion_rapida()
-			AudioManager.stop_respiracion_calmada()
-		last_state = npc_state
-		npc_state = roundi((interpolation) * (sprite_textures.size() - 1))
-	sprite.texture = sprite_textures[npc_state]
+	var new_state = roundi(interpolation * (sprite_textures.size() - 1))
+
+	if new_state > last_state:
+		# Va subiendo -> usar el estado siguiente
+		last_state = new_state
+		sprite.texture = sprite_textures[last_state]
+		AudioManager.play_respiracion_rapida()
+		AudioManager.stop_respiracion_calmada()
+	elif new_state < last_state:
+		# Si baja, volvemos al calmado (índice 0)
+		last_state = 0
+		sprite.texture = sprite_textures[0]
+		AudioManager.play_respiracion_calmada()
+		AudioManager.stop_respiracion_rapida()
